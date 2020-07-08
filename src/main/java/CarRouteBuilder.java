cd5e1c75e09aa3402cc1c6c6781e0de846e6ead0
Gabriel Guimaraes <dedito182@gmail.com>
2020-07-08 19:05:51 -0300

    creating process

--- src/main/java/CarRouteBuilder.java
+++ src/main/java/CarRouteBuilder.java
@@ -1,7 +1,13 @@
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
 public class CarRouteBuilder extends RouteBuilder {
     public void configure() throws Exception {
-        from("file:src/data?noop=true").to("jms:cars2");
+        from("file:src/data?noop=true").to("jms:cars2").process(new Processor() {
+            public void process(Exchange exchange) throws Exception {
+                System.out.println("Name" + exchange.getIn().getHeader("CamelFileName"));
+            }
+        });
     }
 }
